459da7a777a26f426601462bf3a4f5302e473a44
Gagandeep Bhardwaj <gbhardwaj572@gmail.com>
2022-06-22 00:45:26 +0530

    22 June 2022

--- dsa_lovebabr/OOPS_lovebabr/oops-part-1.cpp
+++ dsa_lovebabr/OOPS_lovebabr/oops-part-1.cpp
@@ -1,8 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Hero{
+    private :
+    int health;
+
+    public :
+    char level;
+
+    void Sethealth(int h)
+    {
+        this->health = h;
+    }
+    void Setlevel(char ch)
+    {
+        this->level = ch;
+    }
+    int Gethealth()
+    {
+        return health;
+    }
+    char Getlevel()
+    {
+        return level;
+    }
+};
+
 int main()
 {
-    
+    Hero Gagan;
+    Hero * babbar = new Hero();
+    babbar->Sethealth(90);
+    babbar->Setlevel('A');
+    Gagan.Sethealth(100);
+    Gagan.Setlevel('D');
+    cout<<"Health of Hero 1 is : "<<Gagan.Gethealth()<<endl;
+    cout<<"Level of Hero 1 is : "<<Gagan.Getlevel()<<endl;
+     cout<<"Health of Hero 2 is : "<<babbar->Gethealth()<<endl;
+    cout<<"Level of Hero 2 is : "<<babbar->Getlevel()<<endl;
     return 0;
 }

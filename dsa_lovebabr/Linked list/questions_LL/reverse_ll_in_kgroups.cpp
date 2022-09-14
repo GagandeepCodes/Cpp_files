e7fa81843c815fd58b755391d4b52b7ffb8ecb9a
Gagandeep Bhardwaj <gbhardwaj572@gmail.com>
2022-09-14 22:27:33 +0530

    14 September 2022

--- dsa_lovebabr/Linked list/questions_LL/reverse_ll_in_kgroups.cpp
+++ dsa_lovebabr/Linked list/questions_LL/reverse_ll_in_kgroups.cpp
@@ -31,13 +31,13 @@
 //     Node* prev = NULL;
 //     int count= 0;
     
-//     while( curr != NULL && count < k ) {
-//         next = curr -> next;
-//         curr -> next = prev;
-//         prev = curr;
-//         curr = next;
-//         count++;
-//     }
+    // while( curr != NULL && count < k ) {
+    //     next = curr -> next;
+    //     curr -> next = prev;
+    //     prev = curr;
+    //     curr = next;
+    //     count++;
+    // }
     
 //     //step2: Recursion dekhlega aage ka 
 //     if(next != NULL) {

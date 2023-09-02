fa856fac5f70c920b75c27a3d8974e1411a7f724
Gagandeep Bhardwaj <gbhardwaj572@gmail.com>
2023-09-02 23:12:53 +0530

    codeforces questions pushed

--- codeforces/contest/C_MEX_Repetition.cpp
+++ codeforces/contest/C_MEX_Repetition.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 #define ll long long
 #define rep(i, a, b) for (ll i = a; i < b; i++)
+#define disp(v,st) for(int i=st;i<v.size();i++){cout<<v[i]<<' ';} cout<<endl
 const ll M = 1e9 + 7; // 998244353;
 void solve()
 {
@@ -31,33 +32,20 @@ void solve()
             }
         }
         vector<ll> ans(n + 1, 0);
-        for (int i = 0; i < n; i++)
+        for (ll i = 0; i < n; i++)
         {
             ans[i + 1] = v[i];
         }
         ans[0] = mex;
-        vector<int> res(n + 1, INT_MIN);
-        int j = 0;
+        vector<ll> res;
+        res.push_back(mex);
+        ll j = 0;
         while (j <= n)
         {
-            int x = j + k;
-            if (n<x)
-            {
-                res[(x) % (n + 1)] = ans[j];
-            }
-            else
-            {
-                res[x] = ans[j];
-            }
+            res.push_back(v[(n+1-(k%(n+1))+j)%(n+1)]);
             j++;
         }
-        int i = 1;
-        while (i <= n)
-        {
-            cout << res[i] << " ";
-            i++;
-        }
-        cout << endl;
+        disp(res,1);
     }
 }
 int main()
